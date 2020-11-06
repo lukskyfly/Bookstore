606a03fea6a7bcc903da89fd0f008f7fe2fb5168
lukas <lukasz.grazda@gmail.com>
2020-11-06 11:29:27 +0100

    add test viev in web

--- src/test/java/pl/sda/controller/BookControllerTest.java
+++ src/test/java/pl/sda/controller/BookControllerTest.java
@@ -1,7 +1,9 @@
 package pl.sda.controller;
 
-import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +14,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pl.sda.model.Book;
 import pl.sda.service.BookService;
+
+import java.util.Arrays;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -23,11 +28,19 @@ public class BookControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private BookService bookService;
+    private Book bookTest = new Book(1,"Pan Tadeusz","Adam Mickiewicz", "12345", "opis",1834);
     @Test
     public void bookListTest() throws Exception {
+        //given
+        Mockito.when(bookService.getAll()).thenReturn(Arrays.asList(bookTest));
+
+        //when//then
         mockMvc.perform(MockMvcRequestBuilders.get("/book-list"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("book-list"));
+                .andExpect(MockMvcResultMatchers.view().name("book-list"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("books"))
+                .andExpect(MockMvcResultMatchers.model().attribute("books", Matchers.notNullValue()));
+        Mockito.verify(bookService,Mockito.times(1)).getAll();
 
     }
 }
